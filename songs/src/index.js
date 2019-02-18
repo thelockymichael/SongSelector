039660d2ad95a71f0f2b52dcc2080c51151684f5
thelockymichael <thelockymichael@gmail.com>
2019-02-18 22:50:20 +0200

    asdf

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import reducers from './reducers';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 

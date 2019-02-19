f725816910bf6f1ce7811b8ffd4334015a2128d4
thelockymichael <thelockymichael@gmail.com>
2019-02-19 12:08:56 +0200

    df

--- songs/src/index.js
+++ songs/src/index.js
@@ -6,6 +6,11 @@ import { createStore } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+);
 
 
